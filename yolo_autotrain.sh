beffca91329973976cde6e4ccfedf2ae2abc3290
Daniel Taciak <daniel.taciak@yahoo.de>
2021-08-04 10:04:04 +0200

    Fixed anothertypo

--- yolo_autotrain.sh
+++ yolo_autotrain.sh
@@ -9,7 +9,7 @@ mv data.yaml ../..
 
 cd ../..
 
-python3 detect.py python train.py --img $imgsize --batch $batchsize --epochs $epochs --data data.yaml --weights yolov5s6.pt
+python3 train.py --img $imgsize --batch $batchsize --epochs $epochs --data data.yaml --weights yolov5s6.pt
 
 zip -r results.zip runs
 
