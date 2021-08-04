b6482ed86bf65d97f53ef39cd45ce3a74eb014b9
Daniel Taciak <daniel.taciak@yahoo.de>
2021-08-04 11:21:03 +0200

    Added yaml download

--- yolo_autotrain.sh
+++ yolo_autotrain.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd /usr/src/app/data/images 
+cd /usr/src/app/data/
+rm -rf images
 wget $zipfile -O data.zip
 unzip data.zip
 
 rm data.zip
-mv data.yaml ../..
 
-cd ../..
+cd ..
+
+wget $yaml -O data.yaml
 
 python3 train.py --img $imgsize --batch $batchsize --epochs $epochs --data data.yaml --weights yolov5s6.pt
 
