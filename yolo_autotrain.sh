dca42a28239e4afed1a879c12ce4fbc8c05ab177
Daniel Taciak <daniel.taciak@yahoo.de>
2021-08-04 09:47:47 +0200

    Added yolo auto train script

---
+++ yolo_autotrain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /usr/src/app/data/iamges 
+wget $zipfile -O data.zip
+unzip data.zip
+
+rm data.zip
+mv data.yaml ../..
+
+cd ../..
+
+python3 detect.py python train.py --img $imgsize --batch $batchsize --epochs $epochs --data data.yaml --weights yolov5s6.pt
+
+zip -r results.zip runs
+
+curl --upload-file ./results.zip https://transfer.sh/results.zip
